60d415931ab33de76816768de17495d4175964a3
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2023-03-10 14:47:14 +0530

    Added sample.

---
+++ ValidateEditors/ValidateEditors/ValidateEditors/Source/ItemSourceProvider.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.Maui.DataForm;
+
+namespace ValidateEditors
+{
+    public class ItemSourceProvider : IDataFormSourceProvider
+    {
+        public object GetSource(string sourceName)
+        {
+            if (sourceName == "State")
+            {
+                List<string> state = new List<string>() { "TamilNadu", "Kerala", "Gujarat", "Assam" };
+                return state;
+            }
+
+            if (sourceName == "Country")
+            {
+                List<string> country = new List<string>() { "USA", "Italy", "India", "Indonesia", "Ireland" };
+                return country;
+            }
+
+            if (sourceName == "City")
+            {
+                List<string> city = new List<string>() { "Chennai", "California", "Calabria", "India", "Qatar" };
+                return city;
+            }
+
+            return new List<string>();
+        }
+    }
+}
---
+++ ValidateEditors/ValidateEditors/ValidateEditors/ViewModel/DataFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ValidateEditors
+{
+    public class DataFormViewModel
+    {
+        public DataFormModel DataFormModel { get; set; }
+        public DataFormViewModel()
+        {
+            this.DataFormModel = new DataFormModel();
+        }
+    }
+}
